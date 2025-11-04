23cad93cfa378ed2929fc42ae0c540b7416d091d
dfarinnam01 <dfarinnam01@educarex.es>
2025-11-05 00:24:40 +0100

    Ejercicios Menu corregidos v2

--- actividades/UT-04/actividad_4.1/ejercicio10.py
+++ actividades/UT-04/actividad_4.1/ejercicio10.py
@@ -3,27 +3,28 @@ print("2______________Restar")
 print("3______________Multiplicar")
 print("4______________Dividir")
 
-
-opcion = int(input("Selecciona una opcion: "))
-if opcion in [1, 2, 3, 4]:
-    num1 = int(input("Ingresa un numero: "))
-    num2 = int(input("Ingresa otro numero: "))
-    if opcion == 1:
-        total = num1 + num2
-        print("El resultado es:", total)
-    elif opcion == 2:
-        total = num1 - num2
-        print("El resultado es:", total)
-    elif opcion == 3:
-        total = num1 * num2
-        print("El resultado es:", total)
-    elif opcion == 4:
-        try:
-            total = num1 // num2
+try:
+    opcion = int(input("Selecciona una opcion: "))
+    if opcion in [1, 2, 3, 4]:
+        num1 = int(input("Ingresa un numero: "))
+        num2 = int(input("Ingresa otro numero: "))
+        if opcion == 1:
+            total = num1 + num2
             print("El resultado es:", total)
-        except:
-            print("Error en la division")
-else:
-    print("No has eligido un numero del menu")
-
+        elif opcion == 2:
+            total = num1 - num2
+            print("El resultado es:", total)
+        elif opcion == 3:
+            total = num1 * num2
+            print("El resultado es:", total)
+        elif opcion == 4:
+            try:
+                total = num1 // num2
+                print("El resultado es:", total)
+            except:
+                print("Error en la division")
+    else:
+        print("No has eligido un numero del menu")
+except ValueError:
+    print("Por favor, introduce solo números válidos")
 
--- actividades/UT-04/actividad_4.2.1/ejercicio10.py
+++ actividades/UT-04/actividad_4.2.1/ejercicio10.py
@@ -5,29 +5,31 @@ while opcion !=5:
     print("3______________Multiplicar")
     print("4______________Dividir")
     print("5______________Salir\n")
+    try:
+        opcion = int(input("Selecciona una opcion: "))
+        if opcion in [1,2,3,4]:
+            num1 = int(input("Ingresa un numero: "))
+            num2 = int(input("Ingresa otro numero: "))
 
-    opcion = int(input("Selecciona una opcion: "))
-    if opcion in [1,2,3,4]:
-        num1 = int(input("Ingresa un numero: "))
-        num2 = int(input("Ingresa otro numero: "))
-
-        if opcion == 1:
-            total = num1 + num2
-            print("El resultado es:", total)
-        elif opcion == 2:
-            total = num1 - num2
-            print("El resultado es:", total)
-        elif opcion == 3:
-            total = num1 * num2
-            print("El resultado es:", total)
-        elif opcion == 4:
-            try:
-                total = num1 // num2
+            if opcion == 1:
+                total = num1 + num2
                 print("El resultado es:", total)
-            except:
-                print("Error en la division")
-    elif opcion == 5:
-            print("FIN DEL PROGRAMA")
-    else:
-        print("No has eligido un numero del menu")
+            elif opcion == 2:
+                total = num1 - num2
+                print("El resultado es:", total)
+            elif opcion == 3:
+                total = num1 * num2
+                print("El resultado es:", total)
+            elif opcion == 4:
+                try:
+                    total = num1 // num2
+                    print("El resultado es:", total)
+                except:
+                    print("Error en la division")
+        elif opcion == 5:
+                print("FIN DEL PROGRAMA")
+        else:
+            print("No has eligido un numero del menu")
 
+    except:
+        print("Por favor, introduce solo números válidos")
