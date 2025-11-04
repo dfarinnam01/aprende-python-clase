05130ced4e17ccd99841c3b8c376ee75c897bf83
dfarinnam01 <dfarinnam01@educarex.es>
2025-11-05 00:18:06 +0100

    Ejercicios Menu corregidos

--- actividades/UT-04/actividad_4.1/ejercicio10.py
+++ actividades/UT-04/actividad_4.1/ejercicio10.py
@@ -4,24 +4,25 @@ print("3______________Multiplicar")
 print("4______________Dividir")
 
 
-numero = int(input("Selecciona una opcion: "))
-num1 = int(input("Ingresa un numero: "))
-num2 = int(input("Ingresa otro numero: "))
-if numero == 1:
-    total = num1 + num2
-    print("El resultado es:", total)
-elif numero == 2:
-    total = num1 - num2
-    print("El resultado es:", total)
-elif numero == 3:
-    total = num1 * num2
-    print("El resultado es:", total)
-elif numero == 4:
-    try:
-        total = num1 // num2
+opcion = int(input("Selecciona una opcion: "))
+if opcion in [1, 2, 3, 4]:
+    num1 = int(input("Ingresa un numero: "))
+    num2 = int(input("Ingresa otro numero: "))
+    if opcion == 1:
+        total = num1 + num2
         print("El resultado es:", total)
-    except:
-        print("Error en la division")
+    elif opcion == 2:
+        total = num1 - num2
+        print("El resultado es:", total)
+    elif opcion == 3:
+        total = num1 * num2
+        print("El resultado es:", total)
+    elif opcion == 4:
+        try:
+            total = num1 // num2
+            print("El resultado es:", total)
+        except:
+            print("Error en la division")
 else:
     print("No has eligido un numero del menu")
 
--- actividades/UT-04/actividad_4.2.1/ejercicio10.py
+++ actividades/UT-04/actividad_4.2.1/ejercicio10.py
@@ -1,29 +1,33 @@
-numero=0
-while numero !=5:
+opcion=0
+while opcion !=5:
     print("\n1______________Sumar")
     print("2______________Restar")
     print("3______________Multiplicar")
     print("4______________Dividir")
     print("5______________Salir\n")
 
-    numero = int(input("Selecciona una opcion: "))
-    num1 = int(input("Ingresa un numero: "))
-    num2 = int(input("Ingresa otro numero: "))
-    if numero == 1:
-        total = num1 + num2
-        print("El resultado es:", total)
-    elif numero == 2:
-        total = num1 - num2
-        print("El resultado es:", total)
-    elif numero == 3:
-        total = num1 * num2
-        print("El resultado es:", total)
-    elif numero == 4:
-        try:
-            total = num1 // num2
+    opcion = int(input("Selecciona una opcion: "))
+    if opcion in [1,2,3,4]:
+        num1 = int(input("Ingresa un numero: "))
+        num2 = int(input("Ingresa otro numero: "))
+
+        if opcion == 1:
+            total = num1 + num2
             print("El resultado es:", total)
-        except:
-            print("Error en la division")
+        elif opcion == 2:
+            total = num1 - num2
+            print("El resultado es:", total)
+        elif opcion == 3:
+            total = num1 * num2
+            print("El resultado es:", total)
+        elif opcion == 4:
+            try:
+                total = num1 // num2
+                print("El resultado es:", total)
+            except:
+                print("Error en la division")
+    elif opcion == 5:
+            print("FIN DEL PROGRAMA")
     else:
         print("No has eligido un numero del menu")
 
