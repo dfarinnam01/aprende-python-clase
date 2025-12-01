6b3a9c20f613abf066eda50945ce7f489224fdc1
dfarinnam01 <dfarinnam01@educarex.es>
2025-12-01 19:02:40 +0100

    a

--- actividades/UT-08/ejercicio_sucursal/movimiento.py
+++
@@ -1,9 +0,0 @@
-class Movimiento:
-    def __init__(self, fecha, concepto, tipo, cantidad):
-        self.fecha = fecha
-        self.concepto = concepto
-        self.tipo = tipo
-        self.cantidad = cantidad
-
-    def __str__(self):
-        return f"{self.fecha} | {self.tipo} | {self.concepto} | {self.cantidad}€"
