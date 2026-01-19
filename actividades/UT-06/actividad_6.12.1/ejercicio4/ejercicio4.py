a906e0307af2b79723854b2abbe3c89d6e52c9d3
dfarinnam01 <dfarinnam01@educarex.es>
2026-01-19 19:04:40 +0100

    4

--- actividades/UT-06/actividad_6.12.1/ejercicio4/ejercicio4.py
+++ actividades/UT-06/actividad_6.12.1/ejercicio4/ejercicio4.py
@@ -2,5 +2,6 @@ with open("ejemplo.txt", "r",encoding="UTF-8") as f:
     cl=0
     for linea in f:
         for letra in linea:
-            cl=cl+1
+            if letra!="\n":
+                cl=cl+1
     print(cl)
